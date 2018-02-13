6d113b9d484117dd96e6ea12ec6d8edfa8e2a2e1
mehak16163 <mehak16163@iiitd.ac.in>
2018-02-13 13:36:44 +0530

    some comments

--- src/flight_info_db.java
+++ src/flight_info_db.java
@@ -5,59 +5,59 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-class flight{
+class flight{//flight class
 	int id ;
 	static int count=1;
-	ArrayList<passenger> pass = new ArrayList<passenger>();
+	ArrayList<passenger> pass = new ArrayList<passenger>();//passengers of that flight
 	public flight(){
 		id = count;
 		count++;
 	}
 }
 
-class passenger{
+class passenger{//passenger class
 	int id;
 	static int count=1;
-	ArrayList<flight> fl = new ArrayList<flight>(); 
+	ArrayList<flight> fl = new ArrayList<flight>();//flights of that passenger 
 	public passenger(){
 		id = count;
 		count++;
 	}
 }
 
-abstract class Transaction implements Runnable{
-	flight_info_db db;
+abstract class Transaction implements Runnable{//transacction class which is abstract
+	flight_info_db db;//database of the transaction
 	public Transaction(flight_info_db _db){
 		db = _db;
 	}
 }
 
-class Reserve extends Transaction{
+class Reserve extends Transaction{//Transaction of type reserve
 	int f;
 	int p;
-	public Reserve(flight_info_db _db , int _f , int _p){
+	public Reserve(flight_info_db _db , int _f , int _p){//constructor of the transaction
 		super(_db);
 		f = _f;
 		p = _p;
 	}
-	private Object lock = new Object();
+	private Object lock = new Object();//for implementing 2PL
 	public void run() {
 		flight _f = db.fl[f];
 		passenger _p = db.pass[p];
-		synchronized(lock){
+		synchronized(lock){//giving access to locks
 			_p.fl.add(_f);
 			_f.pass.add(_p);
-		}
+		}//releasing the locks
 	}
 	
 }
 
 
 
-public class flight_info_db {
+public class flight_info_db {//database class
 	
-	static passenger[] pass;
-	static flight[] fl;
+	static passenger[] pass;//list of passengers
+	static flight[] fl;//list of flights
 		
 	public static void main(String[] args) throws NumberFormatException, IOException{
 		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
@@ -76,7 +76,7 @@ public class flight_info_db {
 		System.out.println("Enter no. of transactions: ");
 		int t = Integer.parseInt(rd.readLine());
 		System.out.println("Menu:\n1. Reserve(F,i)\n2. Cancel(F,i)\n3.My_Flights(id)\n4.Total_Reservations()\n5.Transfer(F1,F2,i)\nPlease Enter transaction option and correct arguments.");
-		ExecutorService exec = Executors.newFixedThreadPool(5);
+		ExecutorService exec = Executors.newFixedThreadPool(5);//creating threadpool for concurrency.
 		for (int i=0;i<t;i++){
 			
 		}
