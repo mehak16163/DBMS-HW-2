c05a0a721d9c3c73e634975d6629a93c3acf08f4
mehak16163 <mehak16163@iiitd.ac.in>
2018-02-13 13:44:31 +0530

    implementation with locks

--- src/flight_info_db.java
+++ src/flight_info_db.java
@@ -4,9 +4,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 
 class flight{//flight class
 	int id ;
+	ReentrantLock lock = new ReentrantLock();
 	static int count=1;
 	ArrayList<passenger> pass = new ArrayList<passenger>();//passengers of that flight
 	public flight(){
@@ -16,6 +18,7 @@ class flight{//flight class
 }
 
 class passenger{//passenger class
+	ReentrantLock lock = new ReentrantLock();
 	int id;
 	static int count=1;
 	ArrayList<flight> fl = new ArrayList<flight>();//flights of that passenger 
@@ -27,16 +30,18 @@ class passenger{//passenger class
 
 abstract class Transaction implements Runnable{//transacction class which is abstract
 	flight_info_db db;//database of the transaction
-	public Transaction(flight_info_db _db){
+	boolean mode;
+	public Transaction(flight_info_db _db , boolean m ){
 		db = _db;
+		mode = m;
 	}
 }
 
 class Reserve extends Transaction{//Transaction of type reserve
 	int f;
 	int p;
-	public Reserve(flight_info_db _db , int _f , int _p){//constructor of the transaction
-		super(_db);
+	public Reserve(flight_info_db _db , int _f , int _p , boolean m){//constructor of the transaction
+		super(_db,m);
 		f = _f;
 		p = _p;
 	}
@@ -44,10 +49,19 @@ class Reserve extends Transaction{//Transaction of type reserve
 	public void run() {
 		flight _f = db.fl[f];
 		passenger _p = db.pass[p];
-		synchronized(lock){//giving access to locks
+		if (mode){
+			_p.lock.lock();
+			_p.fl.add(_f);
+			_f.lock.lock();
+			_f.pass.add(_p);
+			_p.lock.unlock();
+			_f.lock.unlock();
+			//releasing the locks
+		}
+		else{
 			_p.fl.add(_f);
 			_f.pass.add(_p);
-		}//releasing the locks
+		}
 	}
 	
 }
@@ -55,7 +69,6 @@ class Reserve extends Transaction{//Transaction of type reserve
 
 
 public class flight_info_db {//database class
-	
 	static passenger[] pass;//list of passengers
 	static flight[] fl;//list of flights
 		
@@ -73,6 +86,11 @@ public class flight_info_db {//database class
 		for (int i=0;i<=flights;i++){
 			fl[i]= new flight();
 		}
+		System.out.println("Enter Mode of execution:\n1. Serially\n2. Concurrently");
+		int m = Integer.parseInt(rd.readLine());
+		boolean mode=false;
+		if (m==2)
+			mode = true;
 		System.out.println("Enter no. of transactions: ");
 		int t = Integer.parseInt(rd.readLine());
 		System.out.println("Menu:\n1. Reserve(F,i)\n2. Cancel(F,i)\n3.My_Flights(id)\n4.Total_Reservations()\n5.Transfer(F1,F2,i)\nPlease Enter transaction option and correct arguments.");
